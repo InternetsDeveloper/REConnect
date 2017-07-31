182cb25d9bcfaf5f2beae8de9aa477c289f80752
murphy-nerdism <markmurphy@nerdism.com>
2017-07-30 23:00:02 -0500

    Office / App Build Add

---
+++ ui/office/app.constants.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('reConnect')
+        .constant('Constants', {
+            apiURL: api + 'api/',
+            DATE_RANGE_CHANGE: 'date_range_change',
+            TABLEHEAD_AUTO_FOCUS_WAIT: 1000,
+            SERVICE_POLLING_INTERVAL: 15000
+        });
+})();
---
+++ ui/office/app.module.js
@@ -0,0 +1,41 @@
+(function () {
+    'use strict';
+
+    /**
+     * @namespace reConnect
+     */
+    angular.module('reConnect', [
+		'ui.router',
+		'ngAnimate',
+		'ngMaterial',
+		'mdPickers',
+		'vAccordion',
+		'ngOpbeat',
+		'formly',
+		'formlyMaterial',
+		'uiGmapgoogle-maps',
+		'reConnect.login',
+		'reConnect.contact',
+		'reConnect.home',
+		'reConnect.task',
+		'reConnect.marketing',
+		'reConnect.report',
+		'reConnect.propertymanagement',
+		'reConnect.fileUpload',
+		'reConnect.callScripts',
+		'reConnect.login',
+		'reConnect.dispatch',
+		'reConnect.customer',
+		'reConnect.serviceRequest',
+		'reConnect.dispatchCode',
+		'reConnect.technician',
+		'reConnect.note',
+		'reConnect.department',
+		'reConnect.product',
+		'reConnect.address',
+		'reConnect.agreements',
+		'reConnect.tags',
+		'reConnect.utils.pushIn',
+		'reConnect.formIntegrity'
+	]);
+})();
